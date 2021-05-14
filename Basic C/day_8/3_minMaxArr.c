4240fe8d1144b234ee22ea4989cde098fd9793fa
shreeshailaya <s.r.vitkar55@gmail.com>
2021-05-15 00:40:56 +0530

    Min Max array added

--- Basic C/day_8/3_minMaxArr.c
+++ Basic C/day_8/3_minMaxArr.c
@@ -1,28 +1,42 @@
 #include<stdio.h>
-int minMax(int []);
+int minMax(int [],int n);
 int main(){
-    int arr[5];
+    int arr[5],n=5;
     printf("\n\n\tEnter the 5 digit of array");
     for(int i=0;i<5;i++)
     {
         scanf("%d",&arr[i]);
     }
-    minMax(arr);
-
+    minMax(arr,n);
     return 0;
 }
-int minMax(int arr[])
+int minMax(int arr[],int n)
 {
-    int min=arr[0],max;
-    for(int i=0;i<5;i++)
+    int max,min;
+    if(arr[0]>arr[1])
     {
-        for(int j=i;j<5;j++)
+        max=arr[0];
+        min=arr[1];
+    }else
+    {
+        max=arr[1];
+        min=arr[0];
+    }
+
+    for(int i=2;i<n;i++)
+    {
+        if(arr[i]>max)
         {
-            if(min<arr[j])
-            {
-                min=arr[j];
-            }
+            //printf("\n\n\t %d is Max",arr[i]);
+            max=arr[i];
+        }else if(arr[i]<min)
+        {
+            min=arr[i];
         }
     }
-    printf("\n\n\t %d",min);
+
+    printf("\n\n\t The maximum value is %d", max);
+    printf("\n\n\t The minimum value is %d", min);
+    printf("\n\n\t");
+
 }
